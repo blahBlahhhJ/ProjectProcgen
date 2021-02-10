4d6ab9b45fb1afefd6e1b3e1daae7f23c3fee723
Jason Wang <blahblahj.wsy@gmail.com>
2021-02-10 00:15:24 -0800

    switch back to gym3 env, add skeleton for training process

--- agents/q_agent.py
+++ agents/q_agent.py
@@ -2,10 +2,11 @@ import argparse
 import numpy as np
 import torch
 import gym3
+from gym3.extract_dict_ob import ExtractDictObWrapper
 from procgen import ProcgenGym3Env
 
 from .utils.schedule import LinearSchedule
-from .utils.wrapper import RgbWrapper
+from .utils.memory import ReplayBuffer
 
 # gym config
 ENV_NAME = 'fruitbot'
@@ -16,12 +17,21 @@ START_LEVEL = 500
 # training config
 TRAIN_STEPS = 4e6
 TRAIN_RESUME = 0
+LEARNING_START = 5e4
+LEARNING_FREQ = 4
+UPDATE_FREQ = 1e4
+SAVING_FREQ = 2.5e5
+BATCH_SIZE = 32
 LR_START = 2.5e-4
 LR_END = 5e-5
 LR_STEPS = 2e6
 EPS_START = 1
 EPS_END = 0.1
 EPS_STEPS = 1e6
+GAMMA = 0.99
+
+# eval config
+EVAL_FREQ = 2.5e5
 
 
 class QAgent():
@@ -29,18 +39,40 @@ class QAgent():
         The agent that performs Q-Learning.
     """
     def __init__(self, model, args):
-        self.env = RgbWrapper(ProcgenGym3Env(
-            args.num_envs, args.env_name, 
+        self.env = ProcgenGym3Env(
+            num=args.num_envs, 
+            env_name=args.env_name, 
             num_levels=args.num_levels, 
             start_level=args.start_level, 
             distribution_mode='easy', 
             render_mode='rgb_array'
-        ))
+        )
+        self.env = ExtractDictObWrapper(self.env, 'rgb')
+
         self.model = model
+        self.memory = ReplayBuffer()
         self.config = args
         self.step = self.config.train_resume
-        self.eps_schedule = LinearSchedule(
+        # if it's a resume, then load model
+        if self.step != 0:
+            self.load()
+
+        self.eps_scheduler = LinearSchedule(
             self.config.eps_start, self.config.eps_end, self.config.eps_steps
+        )
+        # for safety reasons
+        self.eps_scheduler.step(self.step)
+
+        self.lr_schedule = LinearSchedule(
+            self.config.lr_start, self.config.lr_end, self.config.lr_steps
+        )
+        self.optimizer = torch.optim.Adam(
+            self.model.parameters(), 
+            lr=self.lr_schedule(self.step)
+        )
+        self.lr_scheduler = torch.optim.lr_scheduler.LambdaLR(
+            self.optimizer,
+            lr_lambda=self.lr_schedule
         )
 
 
@@ -67,13 +99,12 @@ class QAgent():
             -------
                 the chosen action
         """
-        s = torch.from_numpy(s).permute(0, 3, 1, 2) # (1, C, H, W)
+        s = torch.from_numpy(s.astype('float32')).permute(0, 3, 1, 2) # (1, C, H, W)
         q_vals = self._get_q_values(s).detach().numpy()
-        if not epsilon_greedy or np.random.random() > self.eps_schedule.c:
+        if not epsilon_greedy or np.random.random() > self.eps_scheduler.c:
             return np.argmax(q_vals, axis=1)
         else:
             return np.random.randint(q_vals.shape[1], size=(1, ))
-
 
     def evaluate(self, num_eval=5, render=False):
         """
@@ -91,20 +122,67 @@ class QAgent():
             total_reward = 0
             step = 0
             s = env.observe()[1]
+            print(env.observe()[2])
             while True:
                 a = self.get_action(s, epsilon_greedy=False)
                 env.act(a)
-                r, s, first = env.observe()
+                r, s, done = env.observe()
                 total_reward += r
                 step += 1
-                if first and step != 1:
+                if done:
                     avg_reward += total_reward / num_eval
                     break
             print('\tReward:', total_reward, 'Steps:', step)
         print('Evaluation ... Avg Reward:', avg_reward)
         return total_reward
 
+    def save(self):
+        pass
 
+    def load(self):
+        pass
+
+    def update_network(self):
+        pass
+    
+    def train_step(self):
+        pass
+
+    def train(self):
+        since_last_save = 0
+        since_last_eval = 0
+
+        s = self.env.observe()[1]
+        while self.step < self.config.train_steps:
+            a = self.get_action(s)
+            self.env.act(a)
+            r, sp, d = self.env.observe()
+            self.memory.store(s, a, r, sp, d)
+
+
+
+
+            self.step += 1
+            since_last_save += 1
+            since_last_eval += 1
+
+            if (self.step - self.config.train_resume) >= self.config.learning_start:
+                if self.step % self.config.learning_freq == 0:
+                    self.train_step()
+                    since_last_learn = 0
+                if self.step % self.config.update_freq == 0:
+                    self.update_network()
+                if since_last_save >= self.config.saving_freq:
+                    self.save()
+                    since_last_save = 0
+                if since_last_eval >= self.config.eval_freq:
+                    self.evaluate()
+                    since_last_eval = 0
+
+            self.lr_scheduler.step()
+            self.eps_scheduler.step()
+
+            
 
     @staticmethod
     def add_to_argparse(parser):
@@ -115,14 +193,21 @@ class QAgent():
 
         parser.add_argument('--train_steps', type=int, default=TRAIN_STEPS)
         parser.add_argument('--train_resume', type=int, default=TRAIN_RESUME)
+        parser.add_argument('--learning_start', type=int, default=LEARNING_START)
+        parser.add_argument('--learning_freq', type=int, default=LEARNING_FREQ)
+        parser.add_argument('--update_freq', type=int, default=UPDATE_FREQ)
+        parser.add_argument('--saving_freq', type=int, default=SAVING_FREQ)
+        parser.add_argument('--batch_size', type=int, default=BATCH_SIZE)
 
         parser.add_argument('--lr_start', type=float, default=LR_START)
         parser.add_argument('--lr_end', type=float, default=LR_END)
         parser.add_argument('--lr_steps', type=int, default=LR_STEPS)
-
         parser.add_argument('--eps_start', type=float, default=EPS_START)
         parser.add_argument('--eps_end', type=float, default=EPS_END)
         parser.add_argument('--eps_steps', type=int, default=EPS_STEPS)
+        parser.add_argument('--gamma', type=float, default=GAMMA)
+
+        parser.add_argument('--eval_freq', type=type, default=EVAL_FREQ)
 
         return parser
 
---
+++ agents/utils/memory.py
@@ -0,0 +1,117 @@
+import numpy as np
+# from matplotlib import pyplot as plt
+
+# from trees import SumTree, MinTree
+
+class ReplayBuffer:
+    def __init__(self, mem_size=1e6):
+        self.curr_size = 0
+        self.next_idx = 0
+        self.mem_size = mem_size
+        self.states = []
+        self.actions = []
+        self.rewards = []
+        self.next_states = []
+        self.dones = []
+
+    def store(self, s, a, r, sp, d):
+        if self.curr_size < self.mem_size:
+            self.curr_size += 1
+            self.states.append(s)
+            self.actions.append(a)
+            self.rewards.append(r)
+            self.next_states.append(sp)
+            self.dones.append(d)
+        else:
+            self.states[self.next_idx] = s
+            self.actions[self.next_idx] = a
+            self.rewards[self.next_idx] = r
+            self.next_states[self.next_idx] = sp
+            self.dones[self.next_idx] = d
+        self.next_idx = (self.next_idx + 1) % self.mem_size
+        
+
+    def sample(self, batch_size, random_idxs=None):
+        if random_idxs is None:
+            random_idxs = np.random.randint(self.curr_size, size=batch_size)
+
+        sampled_states = []
+        sampled_actions = []
+        sampled_rewards = []
+        sampled_next_states = []
+        sampled_dones = []
+        for idx in random_idxs:
+            sampled_states.append(self.states[idx])
+            sampled_actions.append(self.actions[idx])
+            sampled_rewards.append(self.rewards[idx])
+            sampled_next_states.append(self.next_states[idx])
+            sampled_dones.append(self.dones[idx])
+
+        return sampled_states, sampled_actions, sampled_rewards, sampled_next_states, sampled_dones
+
+
+# class PrioritizedReplayBuffer(ReplayBuffer):
+#     def __init__(self, mem_size=1000000, alpha=1):
+#         super(PrioritizedReplayBuffer, self).__init__(mem_size)
+#         self.alpha = alpha
+
+#         it_capacity = 1
+#         while it_capacity < mem_size:
+#             it_capacity *= 2
+
+#         self.sum_tree = SumTree(it_capacity)
+#         self.min_tree = MinTree(it_capacity)
+#         self.max_priority = 1.0
+
+#     def store(self, s, a, r, sp, d):
+#         idx = self.next_idx
+#         super().store(s, a, r, sp, d)
+#         self.sum_tree[idx] = self.max_priority ** self.alpha
+#         self.min_tree[idx] = self.max_priority ** self.alpha
+
+#     def sample(self, batch_size, beta=0):
+#         idxs = []
+#         p_total = self.sum_tree.sum(0, self.curr_size - 1)
+#         range_len = p_total / batch_size
+
+#         for i in range(batch_size):
+#             prefix_sum = (np.random.rand() + i) * range_len
+#             idx = self.sum_tree.find_idx(prefix_sum)
+#             idxs.append(idx)
+        
+#         weights = []
+#         p_min = self.min_tree.min() / p_total
+#         max_weight = (p_min * self.curr_size) ** (-beta)
+
+#         for idx in idxs:
+#             p_sample = self.sum_tree[idx] / p_total
+#             weight = (p_sample * self.curr_size) ** (-beta)
+#             weights.append(weight / max_weight)
+        
+#         samples = super().sample(batch_size, idxs)
+#         return (*samples, weights, idxs)
+
+#     def update_priorities(self, idxs, priorities):
+#         for idx, priority in zip(idxs, priorities):
+#             self.sum_tree[idx] = priority ** self.alpha
+#             self.min_tree[idx] = priority ** self.alpha
+
+#             self.max_priority = max(self.max_priority, priority)
+        
+
+# if __name__ == "__main__":
+    # mem = PrioritizedReplayBuffer(mem_size=100, alpha=1)
+    # dic = {}
+    # for i in range(100):
+    #     mem.store(i, i, i, i, i)
+    #     mem.update_priorities([i], [i])
+    #     dic[i] = 0
+
+
+    # for i in range(1000):
+    #     sample_state, *_ = mem.sample(2)
+    #     for s in sample_state:
+    #         dic[s] += 1
+    
+    # plt.bar(range(100), [dic[i] for i in range(100)])
+    # plt.show()
